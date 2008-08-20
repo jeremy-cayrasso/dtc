1524c74a6a26e5c9b8fe03a10282fbc5aa179204
Thomas Goirand <thomas@goirand.fr>
2008-08-21 02:59:27 +0800

    Adapted this script for running on FreeBSD.

--- admin/netusegraph/get_net_usage.sh
+++ admin/netusegraph/get_net_usage.sh
@@ -19,7 +19,11 @@ fi
 
 for IFACE_NAME in $IFACELIST ; do
 #	echo $IFACE_NAME
-	IFACE_TXT=`cat /proc/net/dev | grep ${IFACE_NAME} | cut -f 2 -d':'`
+	if [ `uname -s` = "FreeBSD" ] ; then
+		IFACE_TXT=`netstat -Wnib | grep $1 | grep Link | head -1 | gawk '{ print $7,$10}'`
+	else
+		IFACE_TXT=`cat /proc/net/dev | grep ${IFACE_NAME} | cut -f 2 -d':'`
+	fi
 	BYTES_IN=$(($BYTES_IN + `echo ${IFACE_TXT} | gawk -F ' ' '{print $1}'`))
 	BYTES_OUT=$(($BYTES_OUT + `echo ${IFACE_TXT} | gawk -F ' ' '{print $9}'`))
 done
