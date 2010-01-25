021e6726201307d08a2238ac6f4122585c70e6b2
Thomas Goirand <thomas@goirand.fr>
2010-01-25 12:13:04 +0800

    Increased the max password lenght to 255 chars.

--- shared/drawlib/dtc_functions.php
+++ shared/drawlib/dtc_functions.php
@@ -563,7 +563,7 @@ function isMailPassword($login){
 }
 
 function isDTCPassword($pass){
-	$reg = "^([a-zA-Z0-9]){4,16}\$";
+	$reg = "^([a-zA-Z0-9]){4,255}\$";
 	if(!ereg($reg,$pass))	return false;
 	else			return true;
 }
